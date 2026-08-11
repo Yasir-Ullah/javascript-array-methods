9814f861e2a4a8b1ad41bd66dacb86a6b45e6253
Yasir-Ullah <yasirjsdev@gmail.com>
2026-08-11 16:56:44 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// push()
+// let friuts = ["Apple", "Banana", "Orange"];
+// friuts.push("Grapes");
+// console.log(friuts);
+
+// pop
+// let friuts = ["Apple", "Banana", "Orange"];
+// friuts.pop();
+// console.log(friuts);
+
+// shift
+// let friuts = ["Apple", "Banana", "Orange"];
+// friuts.shift();
+// console.log(friuts);
+
+// unshift
+// let friuts = ["Apple", "Banana", "Orange"];
+// friuts.unshift("Mango");
+// console.log(friuts);
+
+// splice
+// let friuts = ["Apple", "Banana", "Orange"];
+// friuts.splice(1, 1);
+// console.log(friuts);
+
+// slice
+let friuts = ["Apple", "Banana", "Orange", "Mango"];
+let result = friuts.slice(1, 3);
+console.log(result);
