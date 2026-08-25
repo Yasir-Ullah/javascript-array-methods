589a7f29386ec54431b436902dd1e4bec11c1a38
Yasir-Ullah <yasirjsdev@gmail.com>
2026-08-25 20:08:46 +0500

    Fix typo in student object city property and enhance object property access logging

--- object/object.js
+++ object/object.js
@@ -1,8 +1,20 @@
 let student = {
   name: "yasir",
   age: 20,
-  city: "lahor",
+  city: "lahore",
   country: "Pakistan",
 };
 
 console.log(student);
+
+// Use a dot to access an object property.
+console.log(student.name); // name
+console.log(student.city); // lahore
+
+let car = {
+  brand: "Toyota",
+  model: "Corolla",
+  year: 2024,
+};
+
+console.log(car);
