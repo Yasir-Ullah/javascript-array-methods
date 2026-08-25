bfc58195cda1d3423613fbb914ee0eee5675ba61
Yasir-Ullah <yasirjsdev@gmail.com>
2026-08-25 20:23:24 +0500

    Refactor index.html to comment out unused script and update object_of_array.js to log student and company details

--- object/object_of_array.js
+++ object/object_of_array.js
@@ -4,4 +4,15 @@ let student = {
   marks: [85, 90, 95],
 };
 
-console.log(student);
+console.log(student.subjects);
+console.log(student.subjects[1]);
+console.log(student.marks[2]);
+
+// example
+let company = {
+  employees: ["Ali", "Ahmed", "Usman"],
+  departments: ["IT", "HR", "Finance"],
+};
+
+console.log(company.employees[1]); // Ahmed
+console.log(company.departments[0]); // IT
