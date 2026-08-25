59a6976002d9706d26a1f64303f5ba267e2addef
Yasir-Ullah <yasirjsdev@gmail.com>
2026-08-25 20:10:42 +0500

    Add initial student array example and log to console

---
+++ object/array_of_object.js
@@ -0,0 +1,21 @@
+// Example 1: Students
+
+let students = [
+  {
+    name: "Yasir",
+    age: 18,
+    marks: 85,
+  },
+  {
+    name: "Ali",
+    age: 19,
+    marks: 90,
+  },
+  {
+    name: "Ahmed",
+    age: 20,
+    marks: 78,
+  },
+];
+
+console.log(students);
